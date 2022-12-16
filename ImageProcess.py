da64fedd4fe107da972c1e38904a576c3aee25af
Leo <liuqiyucn@gmail.com>
2022-12-15 18:35:21 -0800

    load data

--- ImageProcess.py
+++ ImageProcess.py
@@ -1,1 +1,24 @@
 import numpy
+
+from keras.datasets import mnist
+from matplotlib import pyplot
+
+#loading
+(train_X, train_y), (test_X, test_y) = mnist.load_data()
+
+#shape of dataset
+print(train_X[0].size)
+print('X_train: ' + str(train_X.shape))
+print('Y_train: ' + str(train_y.shape))
+print('X_test:  '  + str(test_X.shape))
+print('Y_test:  '  + str(test_y.shape))
+
+
+#plotting
+from matplotlib import pyplot
+for i in range(2):	
+    pyplot.subplot(330 + 1 + i)
+    pyplot.imshow(train_X[i], cmap=pyplot.get_cmap('gray'))
+    print(train_y[i])
+    pyplot.show()
+ 
